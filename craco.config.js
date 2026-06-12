3ded20d427b48c8db8e77da5354b2a6f809056af
tomalex123 <aleksart13@yandex.ru>
2026-06-13 01:29:52 +0400

    new project dashboard firts steps

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+// craco.config.js
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+export default {
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
+}
