71adcf16357ea20e66618deb583ee70bf2b3c0b3
tomalex123 <aleksart13@yandex.ru>
2026-06-14 00:23:17 +0400

    added new folders ,data folder with data files, starting creation of project,first step App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
 
+import {useEffect} from 'react';
+import { BrowserRouter} from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import './App.css'
+
+
 function App() {
+
+  const activeMenu = true;
+  useEffect(() => {
+      console.log("тик")
+  }, [])
   return (
-    <div className="App text-red-700 bg-gray-600">Hello World</div>
+    <div>     
+      <BrowserRouter>          
+          <div className="flex relative dark:bg-main-dark-bg">
+            <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>           
+              <TooltipComponent content="Settings"  position="Top">
+                <button
+                type="button"
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+                style={{ background: "blue", borderRadius: '50%' }}
+
+                >             
+                  <FiSettings />
+                </button>
+              </TooltipComponent>
+            </div>
+             {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              Sidebar
+            </div>
+          )}
+          </div>          
+      </BrowserRouter>
+    </div>
   )
 }
 
